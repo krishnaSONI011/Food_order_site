59c1e2045114d0a12e256a86cfe7878af419cb0c
krishna soni <soni01krishna@gmail.com>
2023-01-29 00:58:22 +0530

    done with navbar

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//for menu toggle through button---
+let menu =document.querySelector("#menubtn");
+let element =document.querySelector("#nav_links");
+
+menu.onclick= () =>{
+    let leftValue = window.getComputedStyle(element,null).getPropertyValue("left");
+    if(leftValue ==="-500px"){
+        element.style.left ="0px";
+    }
+    else{
+        element.style.left ="-500px";
+    }
+}
+//-----------------------------------------------------
+
